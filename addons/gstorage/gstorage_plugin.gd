8b7f9db7e1b4237949c794de6eb46f8fcd27f9d3
Ultradeviant <salimov29@gmail.com>
2025-03-16 01:17:07 +0100

    Fixed issues

--- addons/gstorage/gstorage_plugin.gd
+++ addons/gstorage/gstorage_plugin.gd
@@ -1,1 +1,8 @@
-extends Node
+@tool
+extends EditorPlugin  # Это теперь чисто плагин для включения
+
+func _enter_tree():
+	print("✅ GStorage Plugin Enabled")
+
+func _exit_tree():
+	print("❌ GStorage Plugin Disabled")
